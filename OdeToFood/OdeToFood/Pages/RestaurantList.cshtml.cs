dc129129ba0bd4adae7d5d5f8580a5ab56e65d90
Dilusha Sandeera Jayampathi <32511955+SandeeraDS@users.noreply.github.com>
2019-02-04 15:11:38 +0530

    06.Razor-Views

---
+++ OdeToFood/OdeToFood/Pages/RestaurantList.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using OdeToFood.Models;
+using OdeToFood.Services;
+
+namespace OdeToFood.Pages
+{
+    public class RestaurantListModel : PageModel
+    {
+        private IRestaurantData _restaurantData;
+
+        public IEnumerable<Restaurant> RestaurantList { get; set; }
+
+        public RestaurantListModel(IRestaurantData restaurantData)
+        {
+            _restaurantData = restaurantData;
+        }
+        public void OnGet()
+        {
+            RestaurantList = _restaurantData.GetAll();
+        }
+    }
+}
--- OdeToFood/OdeToFood/Services/InMemoryRestaurant.cs
+++ OdeToFood/OdeToFood/Services/InMemoryRestaurant.cs
@@ -1,44 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using OdeToFood.Models;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+//using OdeToFood.Models;
 
-namespace OdeToFood.Services
-{
-    public class InMemoryRestaurant : IRestaurantData
-    {
-        private List<Restaurant> _restaurants;
+//namespace OdeToFood.Services
+//{
+//    public class InMemoryRestaurant : IRestaurantData
+//    {
+//        private List<Restaurant> _restaurants;
 
-        public InMemoryRestaurant()
-        {
-            _restaurants = new List<Restaurant>
-            {
-                new Restaurant{Id=1,Name="Uthpala's Pizza"},
-                new Restaurant{Id=2,Name="Pizza Hut"},
-                new Restaurant{Id=3 ,Name="Athur's Pizza"}
-            };
-        }
+//        public InMemoryRestaurant()
+//        {
+//            _restaurants = new List<Restaurant>
+//            {
+//                new Restaurant{Id=1,Name="Uthpala's Pizza"},
+//                new Restaurant{Id=2,Name="Pizza Hut"},
+//                new Restaurant{Id=3 ,Name="Athur's Pizza"}
+//            };
+//        }
 
         
 
-        public IEnumerable<Restaurant> GetAll()
-        {
-            return _restaurants.OrderBy(r => r.Name);
-        }
+//        public IEnumerable<Restaurant> GetAll()
+//        {
+//            return _restaurants.OrderBy(r => r.Name);
+//        }
 
-        public Restaurant Get(int id)
-        {
-            return _restaurants.FirstOrDefault(r => r.Id == id);
-        }
+//        public Restaurant Get(int id)
+//        {
+//            return _restaurants.FirstOrDefault(r => r.Id == id);
+//        }
 
-        public Restaurant Add(Restaurant restaurant)
-        {
+//        public Restaurant Add(Restaurant restaurant)
+//        {
 
-            restaurant.Id = _restaurants.Max(r => r.Id) + 1;
-            _restaurants.Add(restaurant);
-            Console.WriteLine(_restaurants);
-            return restaurant;
-        }
-    }
-}
+//            restaurant.Id = _restaurants.Max(r => r.Id) + 1;
+//            _restaurants.Add(restaurant);
+//            Console.WriteLine(_restaurants);
+//            return restaurant;
+//        }
+//    }
+//}
